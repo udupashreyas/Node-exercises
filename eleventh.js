f9a6acd9ad07f681fe43224eab15e68fb3176053
udupashreyas <udupa_shreyas@yahoo.co.in>
2016-12-13 00:06:51 -0800

    fileserver

---
+++ eleventh.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (request,response) {
+	var src = fs.createReadStream(process.argv[3]);
+	src.pipe(response);
+});
+server.listen(process.argv[2]);
